81041deaa046352b55cadfb166fe7df4c6394aed
yx3017 <yx3017@ic.ac.uk>
2019-11-29 00:47:06 +0000

    SEP-26: Merged hatebase into our databse

--- src/js/background.js
+++ src/js/background.js
@@ -3,6 +3,115 @@
  *  JavaScript file to be run when the extension ins installed/updated
  *******************************************************************************/
 
+/**Here's the hatebase stufffffff**/
+
+//Authentication
+function a(key) {
+    let form = new FormData();
+    form.append("api_key", key);
+
+    let settings = {
+        "async": false,
+        "crossDomain": true,
+        "url": "https://api.hatebase.org/4-4/authenticate",
+        "method": "POST",
+        "headers": {
+            "dataType":"jsonp"
+        },
+        "processData": false,
+        "contentType": false,
+        "mimeType": "multipart/form-data",
+        "data": form
+    }
+
+    let token = "";
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+        token = JSON.parse(response).result["token"];
+        console.log("TOKEN: " + token);
+    });
+
+    if (!token) {
+        console.log("NO TOKEN RECEIVED, let's try again..");
+    }
+    return token;
+}
+
+// Actual word retrieving
+function retrieveWords(tokenReceived, key, page, allWordsReceived) {
+
+    let form = new FormData();
+    form.append("api_key", key);
+    form.append("token", tokenReceived);
+    form.append("page", page.toString());
+
+    let settings = {
+        "async": false,
+        "crossDomain": true,
+        "url": "https://api.hatebase.org/4-4/get_vocabulary",
+        "method": "POST",
+        "headers": {
+            "dataType":"jsonp"
+        },
+        "processData": false,
+        "contentType": false,
+        "mimeType": "multipart/form-data",
+        "data": form
+    };
+
+
+    $.ajax(settings).done(function (response) {
+        const words = JSON.parse(response).result;
+        for (let i = 0; i < words.length; i++) {
+            allWordsReceived.push(words[i]);
+        }
+    });
+
+    if (allWordsReceived.length == 0) {
+        console.log("NO WORDS RECEIVED, let's try again..");
+    }
+
+}
+
+// Returns all the words retrieved
+function returnAllWordsList() {
+    // API keys used for hatebase: Trang's, Yiming's,  Ye's, and Helen's
+    const keyList = ["WnxszacNJAUDfmhaRj4DpAFEuXPBTZUZ", "oxdKzspzxPvsskdMupfHhgbnnRqrWmvt", "HQTBtxnWeNReUZAvfPpoqFrjFeLnoJRg"];
+    let allWordsReceived = [];
+
+    var tokenA = a(keyList[1]);
+
+// The word list has 37 pages
+for (let i = 0; i < 10; i++) {
+    retrieveWords(tokenA, keyList[1], i+1, allWordsReceived);
+}
+    return allWordsReceived;
+}
+
+// Return the category of the offensive word
+function categorize(w) {
+    var t = [];
+    if (w["is_about_class"]) {
+        t = "profanity";
+    } else if (w["is_about_disability"]) {
+        t = "disablist";
+    } else if (w["is_about_ethnicity"]) {
+        t = "xenophobia";
+    } else if (w["is_about_gender"]) {
+        t = "sexual";
+    } else if (w["is_about_nationality"]) {
+        t = "racist";
+    } else if (w["is_about_religion"]) {
+        t = "anti-religion";
+    } else if (w["is_about_sexual_orientation"]) {
+        t = "anti-lgbt";
+    } else {
+        t = "profanity";
+    }
+
+    return t;
+}
+
 /**
  * Parses the CSV file and saves data to chrome storage
  * @param data The CSV data
@@ -11,6 +120,27 @@ const loadWordBank = function(data)
 {
     var words = {};
     $.csv.toArrays(data).forEach((element) => words[element[1]] = element[0]);
+
+    var hatebaseWords = returnAllWordsList();
+
+    //hatebaseWords.forEach((object) => words[object.[]] = categorize(object));
+    if (hatebaseWords.length == 0) {
+        console.log("Don't have words from hatebase");
+    } else {
+        for (let i = 0; i < hatebaseWords.length; i++) {
+            var w = hatebaseWords[i];
+            var term = w["term"];
+            //console.log("term: " + term);
+
+            if (!words.hasOwnProperty(term)) {
+                var type = categorize(w);
+                //console.log(type);
+                words[term] = type;
+            }
+        }
+
+        console.log(words);
+    }
 
     // words.splice(0, 1);
     browser.storage.sync.set({wordBank: words}, function ()
